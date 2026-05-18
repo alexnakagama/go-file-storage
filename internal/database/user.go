1fd07500c41eaf952c16c2c17ec2977b4c8ef853
alexnakagama <alexnakagama4@gmail.com>
2026-05-18 20:28:57 -0300

    created function to create user in db

--- internal/database/user.go
+++ internal/database/user.go
@@ -1,6 +1,9 @@
 package database
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type User struct {
 	ID             int       `json:"id"`
@@ -19,4 +22,23 @@ type UserResponse struct {
 	IsAdmin   bool      `json:"is_admin"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func CreateUser(db *sql.DB, name, email, hashedPassword string, isAdmin bool) (*User, error) {
+	now := time.Now()
+	var user User
+
+	query := `
+		INSERT INTO users (name, email, hashed_password, is_admin, created_at, updated_at)
+		VALUES($1, $2, $3, $4, $5, $6)
+		RETURNING id, name, email, is_admin, created_at, updated_at
+	`
+	err := db.QueryRow(query, name, email, hashedPassword, isAdmin, now, now).
+		Scan(&user.ID, &user.Name, &user.Email, &user.HashedPassword, &user.IsAdmin, &user.CreatedAt, &user.UpdatedAt)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
