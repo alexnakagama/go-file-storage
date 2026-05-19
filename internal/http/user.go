0cd0ab918f79602b14c18eb636af0823d37cac17
alexnakagama <alexnakagama4@gmail.com>
2026-05-19 02:20:10 -0300

    added structs for the requests

--- internal/http/user.go
+++ internal/http/user.go
@@ -5,6 +5,18 @@ import (
 	"net/http"
 )
 
+type RegisterRequest struct {
+	Name            string `json:"name"`
+	Email           string `json:"email"`
+	Password        string `json:"password"`
+	ConfirmPassword string `json:"confirm_password"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 func RegisterHandler(db *sql.DB) http.HandlerFunc {
 
 }
