9c46432f6d8fa7135141e52815d8a384c06492e7
alexnakagama <alexnakagama4@gmail.com>
2026-05-19 02:11:56 -0300

    added file for manage the user hanlder

---
+++ internal/http/user.go
@@ -0,0 +1,18 @@
+package http
+
+import (
+	"database/sql"
+	"net/http"
+)
+
+func RegisterHandler(db *sql.DB) http.HandlerFunc {
+
+}
+
+func LoginHandler(db *sql.DB) http.HandlerFunc {
+
+}
+
+func DeleteUserHandler(db *sql.DB) http.HandlerFunc {
+
+}
