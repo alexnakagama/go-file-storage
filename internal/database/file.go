f3b6dbd0e5adeaecf0e99ef80187c5690a5fc33e
alexnakagama <alexnakagama4@gmail.com>
2026-05-20 15:58:32 -0300

    created function to pass the file to a fileresponse

--- internal/database/file.go
+++ internal/database/file.go
@@ -13,9 +13,21 @@ type File struct {
 }
 
 type FileResponse struct {
+	ID        int       `json:"id"`
 	OwnerID   int       `json:"owner_id"`
 	FileName  string    `json:"file_name"`
 	Size      int64     `json:"size"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (f *File) ToResponse() *FileResponse {
+	return &FileResponse{
+		ID:        f.ID,
+		OwnerID:   f.OwnerID,
+		FileName:  f.FileName,
+		Size:      f.Size,
+		CreatedAt: f.CreatedAt,
+		UpdatedAt: f.UpdatedAt,
+	}
 }
