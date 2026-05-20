ca9c884ebff9f5ad16474393caa2ea3d5ef24d70
alexnakagama <alexnakagama4@gmail.com>
2026-05-20 01:49:07 -0300

    renamed http to handler and created server

--- cmd/go-file-storage/main.go
+++ cmd/go-file-storage/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/alexnakagama/go-file-storage/internal/handler"
+	"github.com/alexnakagama/go-file-storage/internal/middleware"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	db, err := sql.Open("postgres", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	http.HandleFunc("/register", handler.RegisterUserHandler(db))
+	http.HandleFunc("/login", handler.LoginUserHandler(db))
+	http.Handle("/delete", middleware.AuthMiddleware(http.HandlerFunc(handler.DeleteUserHandler(db))))
+
+	log.Println("Server running in port:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
