97ea9107694f977913a70843faa20fa75a54c379
alexnakagama <alexnakagama4@gmail.com>
2026-05-20 19:27:13 -0300

    created a struct to manage the response of the files

---
+++ internal/handler/file.go
@@ -0,0 +1,18 @@
+package handler
+
+import "time"
+
+type FileResponse struct {
+	ID        int       `json:"id"`
+	OwnerID   int       `json:"owner_id"`
+	FileName  string    `json:"file_name"`
+	Size      int64     `json:"size"`
+	MimeType  string    `json:"mime_type"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Message   string    `json:"message"`
+}
+
+func AddFileHandler() {
+
+}
